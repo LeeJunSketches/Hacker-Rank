80079677ddc9422a0f127cb92b380cba88ef8ce6
LeeJun <alex.leejun@zrp.com.br>
2021-10-12 15:42:00 -0300

    feat: add challenge 6

---
+++ Challenge_06.ts
@@ -0,0 +1,103 @@
+/*
+
+Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with  places after the decimal.
+
+Note: This challenge introduces precision problems. The test cases are scaled to six decimal places, though answers with absolute error of up to  are acceptable.
+
+Example
+
+There are  elements, two positive, two negative and one zero. Their ratios are ,  and . Results are printed as:
+
+0.400000
+0.400000
+0.200000
+Function Description
+
+Complete the plusMinus function in the editor below.
+
+plusMinus has the following parameter(s):
+
+int arr[n]: an array of integers
+Print
+Print the ratios of positive, negative and zero values in the array. Each value should be printed on a separate line with  digits after the decimal. The function should not return a value.
+
+Input Format
+
+The first line contains an integer, , the size of the array.
+The second line contains  space-separated integers that describe .
+
+Constraints
+
+
+
+Output Format
+
+Print the following  lines, each to  decimals:
+
+proportion of positive values
+proportion of negative values
+proportion of zeros
+Sample Input
+
+STDIN           Function
+-----           --------
+6               arr[] size n = 6
+-4 3 -9 0 4 1   arr = [-4, 3, -9, 0, 4, 1]
+Sample Output
+
+0.500000
+0.333333
+0.166667
+
+*/
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString: string = '';
+let inputLines: string[] = [];
+let currentLine: number = 0;
+
+process.stdin.on('data', function (inputStdin: string): void {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function (): void {
+    inputLines = inputString.split('\n');
+    inputString = '';
+
+    main();
+});
+
+function readLine(): string {
+    return inputLines[currentLine++];
+}
+
+/*
+ * Complete the 'plusMinus' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function plusMinus(arr: number[]): void {
+    const total = arr.length;
+    const positives = arr.filter(i => i > 0);
+    const negatives = arr.filter(i => i < 0)
+    const zeros = arr.filter(i => i === 0);
+
+    const positiveRatio = (positives.length / total).toFixed(6);
+    const negativeRatio = (negatives.length / total).toFixed(6);
+    const zeroRatio = (zeros.length / total).toFixed(6);
+
+    console.log(positiveRatio);
+    console.log(negativeRatio);
+    console.log(zeroRatio)
+}
+
+function main() {
+    const n: number = parseInt(readLine().trim(), 10);
+
+    const arr: number[] = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    plusMinus(arr);
+}
